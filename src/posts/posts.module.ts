0f4b65e95506dddad79b1155595062a60488c400
sef951@gmail.com <sef951@gmail.com>
2024-02-06 22:34:02 +0900

    ➕ Add typeorm options

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PostsService } from './posts.service';
 import { PostsController } from './posts.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PostsModel } from './entities/post.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([PostsModel])],
   controllers: [PostsController],
   providers: [PostsService],
 })
