d662fc54a6b52d923154bb93e4d6492171e03e4e
sef951@gmail.com <sef951@gmail.com>
2024-04-09 14:01:52 +0900

    🎨 Improve SocketIO

--- src/chats/chats.gateway.ts
+++ src/chats/chats.gateway.ts
@@ -2,6 +2,8 @@ import {
   ConnectedSocket,
   MessageBody,
   OnGatewayConnection,
+  OnGatewayDisconnect,
+  OnGatewayInit,
   SubscribeMessage,
   WebSocketGateway,
   WebSocketServer,
@@ -13,36 +15,109 @@ import { ChatsService } from './chats.service';
 import { EnterChatDto } from './dto/enter-chat.dto';
 import { CreateMessagesDto } from './messages/dto/crate-messages.dto';
 import { ChatsMessagesService } from './messages/messages.service';
+import {
+  UseFilters,
+  UseGuards,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { SocketCatchHttpExceptionFilter } from 'src/common/exception-filter/socket-catch-http.exception-filter';
+import { SocketBearerTokenGuard } from 'src/auth/guard/socket/socket-bearer-token.guard';
+import { UsersModel } from 'src/users/entities/users.entity';
+import { UsersService } from 'src/users/users.service';
+import { AuthService } from 'src/auth/auth.service';
 
 @WebSocketGateway({
   // ws://localhost:3000/chats
   namespace: 'chats',
 })
-export class ChatsGateway implements OnGatewayConnection {
+export class ChatsGateway
+  implements OnGatewayConnection, OnGatewayInit, OnGatewayDisconnect
+{
   constructor(
     private readonly chatsService: ChatsService,
     private readonly messagesService: ChatsMessagesService,
+    private readonly userService: UsersService,
+    private readonly authService: AuthService,
   ) {}
 
   @WebSocketServer()
   server: Server;
 
-  handleConnection(socket: Socket) {
-    console.log(`on connect called : ${socket.id}`);
+  afterInit(server: any) {
+    // gateway 연결되기 전에 실행
+    console.log('after gateway init');
   }
 
+  handleDisconnect(socket: Socket) {
+    // 연결이 끊겼을 때 실행
+    console.log(`on disconnect called : ${socket.id}`);
+  }
+
+  async handleConnection(socket: Socket & { user: UsersModel }) {
+    console.log(`on connect called : ${socket.id}`);
+
+    const user = await this.userService.getUserByEmail('aa@ellie.co.kr');
+
+    const headers = socket.handshake.headers;
+
+    const rawToken = headers['authorization'];
+
+    if (!rawToken) {
+      throw new WsException('토큰이 없습니다.');
+    }
+
+    try {
+      const token = this.authService.extractTokenFromHeader(rawToken, true);
+
+      const payload = this.authService.verifyToken(token);
+
+      const user = await this.userService.getUserByEmail(payload.email);
+
+      socket.user = user; // socket이 연결이 지속되는 동안 메세지에서 계속 가지고 있음
+
+      return true;
+    } catch (err) {
+      socket.disconnect();
+    }
+  }
+
+  @UsePipes(
+    new ValidationPipe({
+      // main.ts의 글로벌이 REST API에만 적용됨으로 따로 적용 필요
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  )
+  @UseFilters(SocketCatchHttpExceptionFilter)
   @SubscribeMessage('create_chat')
   async createChat(
     @MessageBody() data: CreateChatDto,
-    @ConnectedSocket() socket: Socket,
+    @ConnectedSocket() socket: Socket & { user: UsersModel },
   ) {
     const chat = await this.chatsService.createChat(data);
   }
 
+  @UsePipes(
+    new ValidationPipe({
+      // main.ts의 글로벌이 REST API에만 적용됨으로 따로 적용 필요
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  )
+  @UseFilters(SocketCatchHttpExceptionFilter)
   @SubscribeMessage('enter_chat')
   async enterChat(
     @MessageBody() data: EnterChatDto,
-    @ConnectedSocket() socket: Socket,
+    @ConnectedSocket() socket: Socket & { user: UsersModel },
   ) {
     for (const chatId of data.chatIds) {
       const exists = await this.chatsService.checkIfChatExists(chatId);
@@ -58,10 +133,22 @@ export class ChatsGateway implements OnGatewayConnection {
     socket.join(data.chatIds.map((x) => x.toString()));
   }
 
+  @UsePipes(
+    new ValidationPipe({
+      // main.ts의 글로벌이 REST API에만 적용됨으로 따로 적용 필요
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  )
+  @UseFilters(SocketCatchHttpExceptionFilter)
   @SubscribeMessage('send_message')
   async sendMessage(
     @MessageBody() dto: CreateMessagesDto,
-    @ConnectedSocket() socket: Socket,
+    @ConnectedSocket() socket: Socket & { user: UsersModel },
   ) {
     const chatExists = await this.chatsService.checkIfChatExists(dto.chatId);
 
@@ -69,7 +156,10 @@ export class ChatsGateway implements OnGatewayConnection {
       throw new WsException(`존재하지않는 채팅방입니다. chatId: ${dto.chatId}`);
     }
 
-    const message = await this.messagesService.createMessage(dto);
+    const message = await this.messagesService.createMessage(
+      dto,
+      socket.user.id,
+    );
 
     socket
       .to(message.chat.id.toString()) // socket을 사용하면 나를 제외한 사용자에게 보내짐
--- src/chats/chats.module.ts
+++ src/chats/chats.module.ts
@@ -8,13 +8,20 @@ import { CommonModule } from 'src/common/common.module';
 import { ChatsMessagesService } from './messages/messages.service';
 import { MessagesModel } from './messages/entity/messages.entity';
 import { MessagesController } from './messages/messages.controller';
+import { AuthModule } from 'src/auth/auth.module';
+import { UsersModule } from 'src/users/users.module';
+import { JwtModule } from '@nestjs/jwt';
+import { AuthService } from 'src/auth/auth.service';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([ChatsModel, MessagesModel]),
+    JwtModule.register({}),
     CommonModule,
+    AuthModule,
+    UsersModule,
   ],
   controllers: [ChatsController, MessagesController],
-  providers: [ChatsGateway, ChatsService, ChatsMessagesService],
+  providers: [ChatsGateway, ChatsService, ChatsMessagesService, AuthService],
 })
 export class ChatsModule {}
